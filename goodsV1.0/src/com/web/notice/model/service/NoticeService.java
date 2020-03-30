eaa79bc53f2adea63e1ba33b256d06223f85ae8f
61423 <61423@DESKTOP-M6JJ7H8>
2020-03-30 15:15:37 +0900

    총명 #13 0330 form태그 멀티서브밋,글쓰기,수정하기로직
    공지사항에서 form태그 멀티서브밋설정하고
    공지사항 글쓰기에 파일 다중업로드 처리
    수정하기 서블릿 연결!!

--- goodsV1.0/src/com/web/notice/model/service/NoticeService.java
+++ goodsV1.0/src/com/web/notice/model/service/NoticeService.java
@@ -65,4 +65,16 @@ public class NoticeService {
 		close(conn);
 		return count;
 	}
+	
+	public int insertNotice(Notice n) {
+		Connection conn=getConnection();
+		int result=dao.insertNotice(conn,n);
+		if(result>0) commit(conn);
+		else rollback(conn);
+		close(conn);
+		return result;
+	}
+	
+	
+	
 }
