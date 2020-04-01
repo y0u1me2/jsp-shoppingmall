479696eaabae7b6ce454684fcc260d69e62f564a
61423 <61423@DESKTOP-M6JJ7H8>
2020-04-01 10:23:15 +0900

    총명 #17 0401 조회수 기능구현
    리스트뷰에 조회수 기능추가
    MVC패턴에 적용 완료,properties수장

--- goodsV1.0/src/com/web/notice/controller/NoticeViewServlet.java
+++ goodsV1.0/src/com/web/notice/controller/NoticeViewServlet.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.web.notice.model.service.NoticeService;
 import com.web.notice.model.vo.Notice;
@@ -33,7 +35,33 @@ public class NoticeViewServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		int nNo=Integer.parseInt(request.getParameter("nNo"));
 		
-		Notice n=new NoticeService().oneNotice(nNo);
+		//쿠키가져와서 검사하기
+		Cookie[] cookies=request.getCookies();
+		String val="";
+		boolean hasRead=false;
+		
+		//쿠키가 있는지 없는지 확인(읽었는지 안읽었는지)
+		if(cookies!=null) {
+			for(Cookie c : cookies) {
+				String name=c.getName();
+				String v=c.getValue();
+				if("noticeView".equals(name)) {
+					val=v;
+					if(v.contains("|"+nNo+"|")) {
+						hasRead=true;
+						break;
+					}
+				}
+			}
+		}
+		//안읽엇으면
+		if(!hasRead) {
+			Cookie c=new Cookie("noticeView","|"+nNo+"|");
+			c.setMaxAge(-1);
+			response.addCookie(c);
+		}
+		
+		Notice n=new NoticeService().oneNotice(nNo,hasRead);
 		
 		request.setAttribute("notice",n);
 		request.getRequestDispatcher("/views/client/notice/noticeView.jsp").forward(request, response);
