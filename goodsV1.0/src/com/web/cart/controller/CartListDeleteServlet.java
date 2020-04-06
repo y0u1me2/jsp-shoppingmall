738ebf13b2be3161b52a8c06ab6764722080cafe
승원 <@192.168.0.38>
2020-04-06 15:03:07 +0900

    #24 승원 장바구니 삭제,선택삭제 수정(마무리) 0406
    장바구니 마무리

--- goodsV1.0/src/com/web/cart/controller/CartListDeleteServlet.java
+++ goodsV1.0/src/com/web/cart/controller/CartListDeleteServlet.java
@@ -21,92 +21,100 @@ import com.web.cart.model.vo.Cart;
 @WebServlet("/cart/cartListDelete")
 public class CartListDeleteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CartListDeleteServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public CartListDeleteServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		String cNo = request.getParameter("cLDeleteNo");
-		System.out.println("삭제할 쿠키 : "+cNo);
-		
+		System.out.println("삭제할 쿠키 : " + cNo);
+
 		String cartCook = "";
-		Cookie[] cookies = request.getCookies(); //null이거나 쿠키배열
-		for(Cookie c : cookies) {
-			if(c.getName().equals("cart")) {
-				cartCook=c.getValue();
+		Cookie[] cookies = request.getCookies(); // null이거나 쿠키배열
+		for (Cookie c : cookies) {
+			if (c.getName().equals("cart")) {
+				cartCook = c.getValue();
 			}
 		}
-		String[] cartCooks=cartCook.split("\\|");
+		String[] cartCooks = cartCook.split("\\|");
 		List<String> list = new ArrayList(Arrays.asList(cartCooks));
-		System.out.println("최초 쿠키값 리스트 : "+list);
-		for(int i=0;i<list.size();i++) {
-			if(list.get(i).equals(cNo)) {
-				System.out.println("지울 쿠키값(선택된거) : "+cNo);
+		System.out.println("최초 쿠키값 리스트 : " + list);
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i).equals(cNo)) {
+				System.out.println("지울 쿠키값(선택된거) : " + cNo);
 				list.remove(i);
 			}
 		}
 		list.toArray(cartCooks);
-		System.out.println("리무브된 리스트 값 : "+list);
-		System.out.println("리무브된 리스트 사이즈값 : "+list.size());
 		
+		if(list.size()>0) {
+			if(list.get(0)=="") {
+				list.clear();
+			}
+		}
+		
+		
+		System.out.println("리무브된 리스트 값 : " + list);
+		System.out.println("리무브된 리스트 사이즈값 : " + list.size());
 		String[] cookList = new String[list.size()];
-		String cookL = "";		
-		for(int i=0;i<list.size();i++) {
-			
-			cookList[i]=list.get(i);
-			System.out.println(cookList[i]);
-			System.out.println("리무브된 리스트 값->배열로 옮긴값 : "+cookList[i]);
-			if(list.size()-1==i) {
-				cookL+=cookList[i];
-			}else {
-				cookL+=cookList[i]+"|";
+		String cookL = "";
+
+		for (int i = 0; i < list.size(); i++) {
+			cookList[i] = list.get(i);
+			System.out.println("리무브된 리스트 값->배열로 옮긴값 : " + cookList[i]);
+			if (list.size() - 1 == i) {
+				cookL += cookList[i];
+			} else {
+				cookL += cookList[i] + "|";
 			}
-			
+
 		}
-		
+
 		Cookie cookie = null;
-		if(list.size()!=0) {
-				for(int i=0; i<cookies.length; i++) {
-					if(cookies[i].getName().equals("cart")) {
-							cookie = new Cookie("cart",cookL);				
-							cookie.setPath("/");
-							cookie.setMaxAge(60*60*24*90);
-							response.addCookie(cookie);
-					}
-					
+		if (list.size() != 0) {
+			for (int i = 0; i < cookies.length; i++) {
+				if (cookies[i].getName().equals("cart")) {
+					cookie = new Cookie("cart", cookL);
+					cookie.setPath("/");
+					cookie.setMaxAge(60 * 60 * 24 * 90);
+					response.addCookie(cookie);
 				}
-		}else {
-			for(int i=0; i<cookies.length; i++) {
-				if(cookies[i].getName().equals("cart")) {
-				cookie = new Cookie("cart","");
-				cookie.setPath("/");
-				cookie.setMaxAge(0);
-				response.addCookie(cookie);
+
+			}
+		} else {
+			for (int i = 0; i < cookies.length; i++) {
+				if (cookies[i].getName().equals("cart")) {
+					cookie = new Cookie("cart", "");
+					cookie.setPath("/");
+					cookie.setMaxAge(0);
+					response.addCookie(cookie);
 				}
-				
+
 			}
 		}
-		
-		List<Cart> c = new CartService().searchCart(cookList); 
-		
+
+		List<Cart> c = new CartService().searchCart(cookList);
 		request.setAttribute("cart", c);
 		request.getRequestDispatcher("/views/client/cart/cart.jsp").forward(request, response);
-		
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
